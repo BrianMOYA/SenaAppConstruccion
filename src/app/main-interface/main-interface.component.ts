d6af33e06487f68b1576218ec20187ba2a5e1f89
Brian Moya <bsmoya1@misena.edu.co>
2022-08-23 00:31:58 -0500

    Brian/MainInterface
    Realizo cambios en la estructura de la interfaz (Main interface), tanto en el scss como el ionic

--- src/app/main-interface/main-interface.component.ts
+++ src/app/main-interface/main-interface.component.ts
@@ -56,6 +56,9 @@ export class MainInterfaceComponent implements OnInit {
     //this.moduleNameService.currentModuleName.subscribe(moduleName => this.moduleName = moduleName)
   }
 
+  goToSubject(){
+
+  }
 
 
 
