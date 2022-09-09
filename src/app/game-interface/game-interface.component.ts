cb4fe2a2cf2896bead455625d03f387b92ad262a
Brian Moya <bsmoya1@misena.edu.co>
2022-09-08 20:58:54 -0500

    Brian/Cambios Json
    Realizo cambios en la base de datos, agrego nuevas carpetas con piezas

--- src/app/game-interface/game-interface.component.ts
+++ src/app/game-interface/game-interface.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ModuleNameService } from '../services/module-name.service'
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-game-interface',
@@ -6,19 +8,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./game-interface.component.scss'],
 })
 export class GameInterfaceComponent implements OnInit {
+  moduleName : string;
 
-  public mouduleGames : {img: string, route: string}[] = [
+  public moduleGames : {img: string, route: string}[] = [
     {
-      img: "../../assets/svg/sourceGame/vulnerabilidad/menuGame/juegoCementos.svg",
+      img: "../../assets/svg/sourceGame/menuGame/juegoCementos.svg",
       route: "/gameSelectConcrete"
     },
     {
-      img: "../../assets/svg/sourceGame/vulnerabilidad/menuGame/juegoMalasPracticas.svg",
+      img: "../../assets/svg/sourceGame/menuGame/juegoMalasPracticas.svg",
       route: "/gameChooseDescription"
     },
   ];
 
-  constructor() { }
+  constructor(
+    private moduleNameService: ModuleNameService,
+    private route: ActivatedRoute
+  ) {
+    this.route.params.subscribe(({id}) => {
+
+
+      const moduleId = id === ':id' ? localStorage.getItem('currentModule') : id;
+
+      if(!moduleId.includes("Vulnerabiliad Sismica")){
+        this.moduleGames[0].img = "../../assets/svg/sourceGame/menuGame/portadaJuegoPlanos.svg";
+        this.moduleGames[0].route = "/gameDashboard";
+        this.moduleGames[1].img = "../../assets/svg/sourceGame/menuGame/portadaJuegoSimbolos.svg";
+        this.moduleGames[1].route = "/gameSelectPair";
+      }
+      this.moduleNameService.passModuleName(moduleId);
+    })
+  }
 
   ngOnInit() {}
 
